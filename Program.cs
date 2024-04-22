73940b53c3bb0571554c8ad77cd7b2c6fcd57437
MACGUYVERDK <macguyverdk@hotmail.com>
2024-04-22 14:02:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+var number1 = 5;
+var number2 = 10;
+
+var name = "Adam";
+var age = 33;
+
+
+bool isActive = true;
+if(age > 50)
+{
+    if (name == "Adam") ;
+    {
+        Console.WriteLine("Jestem Adamem Lub Mam Mniej Niz 50 Lat.");
+    }
+     else
+    {
+
+    }
+}
+else
+{
+    Console.WriteLine("40-50 latek");
+}
