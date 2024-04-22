4a1509f7a72d038cfdd9144da83956f26cc34745
MACGUYVERDK <macguyverdk@hotmail.com>
2024-04-22 15:46:29 +0200

    CallengeApp2_expanding3

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
 ﻿
-string name = "Ewa";
-var sex = "woman";
-var age = 25;
 
-if (sex == "woman" && age < 30)
+int number = 6529985;
+string numberString = number.ToString();
+char[] letters = numberString.ToCharArray();
+Console.WriteLine("Example number " + number);
+
+List<int> counters = new List<int>(10);
+
+
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Kobieta ponizej 30 lat");
+    counters.Add(0);
 }
-else if (name == "Ewa" && age == 30)
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Ewa, lat 30");
+    if (char.IsDigit(letter))
+    {
+        int digit = letter - '0';
+        counters[digit]++;
+    }
 }
-else if (age < 18 && sex == "man")
+
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Niepelnoletni mezczyzna");
+    Console.WriteLine(i + " = " + counters[i]);
 }
